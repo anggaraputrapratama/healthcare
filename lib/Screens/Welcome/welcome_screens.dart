846ab72aafd79d2be3fbd1f4865161640f90d8d6
Anggara Putra Pratama <anggaraputraa.pratama@gmail.com>
2020-12-03 11:28:35 +0700

    Tambah logo dan button signup

--- lib/Screens/Welcome/welcome_screens.dart
+++ lib/Screens/Welcome/welcome_screens.dart
@@ -1,35 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare/Screens/Signup/signup_screen.dart';
+import 'package:healthcare/components/rounded_button.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  final Widget child;
+  const WelcomeScreen({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 90),
-            Text.rich(TextSpan(
-              text: 'Welcome',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                  color: HexColor("#646AFA")),
-            )),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text.rich(TextSpan(
-                  text: 'Please login or signup to continue using our app.',
-                  style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
-                ))
-              ],
-            ),
-            SizedBox(height: 40),
-          ],
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Container(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: size.height * 0.10),
+              Text(
+                "Welcome",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                    color: HexColor("#646AFA")),
+              ),
+              Text(
+                " Please login or signup to continue using our app.",
+                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
+              ),
+              SizedBox(height: size.height * 0.05),
+              Image.asset("assets/images/Logo Health Tracker.png"),
+              RoundedButton(
+                text: "Sign Up",
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SignUpScreen();
+                      },
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
