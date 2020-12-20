5bacd2ed2d6c1e978321f8760f3e3340e1ce3a26
Bangtre <trheamadiazulsasurianata@gmail.com>
2020-12-20 22:04:46 +0700

    Fitur Maps

---
+++ lib/Screens/Maps/Tracking.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './tracking_screen.dart';
+import 'package:flutter/services.dart';
+
+//File Start Pertama kali, Pemanggilan Screen.dart
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return MaterialApp(
+      title: "Health App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ScreenPage(),
+    );
+  }
+}
